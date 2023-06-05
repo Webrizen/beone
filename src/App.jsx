e5a889db70ef225aa3a60adde7ce89531572b6a4
Webrizen <webrizen@gmail.com>
2023-06-05 10:56:03 +0530

    make some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <>
     <div className="App">
+      <Navbar/>
       <h1>Lorem ipsum dolor sit amet consectetur.</h1>
     </div>
     </>
