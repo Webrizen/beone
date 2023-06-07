e922b7d1fdefcb998bedd1db95760802c9137838
Webrizen <webrizen@gmail.com>
2023-06-07 15:39:49 +0530

    yoo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import Navbar from './components/navbar';
+import React from "react";
+import Button from "@mui/material/Button";
+import Navbar from "./components/navbar";
+import "./styles/app.css";
 
 function App() {
   return (
     <>
-    <div className="App">
-      <Navbar/>
-      <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-    </div>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <h1>
+            Welcome Back
+            <br />
+            Login <span>To Continue</span>.
+          </h1>
+          <p>
+            The seconds, minutes, and hours slip away,
+            <br />
+            each one bringing us closer to the future and farther from the past
+          </p><br />
+          <a href="#">Login</a>
+          <div className="bg"></div>
+        </div>
+      </div>
     </>
   );
 }
