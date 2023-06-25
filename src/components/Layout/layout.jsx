3c8fc6d3d71ebc838f045a1227108c6404265ffc
WebRizen <webrizen@gmail.com>
2023-06-25 07:20:10 +0530

    something great added

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -4,7 +4,7 @@ import "../../styles/dashboard.css";
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import VerticalStepper from "../verticalstepper";
 import UserOrders from "../userOrders";
-import { Skeleton } from "@mui/material";
+import { Skeleton, Snackbar } from "@mui/material";
 
 const Layout = ({ children }) => {
   function ToggleLeftSideBar() {
@@ -17,13 +17,43 @@ const Layout = ({ children }) => {
   }
 
   const [loading, setLoading] = useState(true);
+  const [slowInternet, setSlowInternet] = useState(false);
+  const [noInternet, setNoInternet] = useState(false);
 
   useEffect(() => {
     // Simulate loading data
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setLoading(false);
     }, 2000);
+
+    // Check internet connection
+    const checkInternetConnection = () => {
+      if (!navigator.onLine) {
+        setNoInternet(true);
+      }
+    };
+
+    // Check for slow internet after 3 seconds
+    const slowInternetTimeout = setTimeout(() => {
+      checkInternetConnection();
+      if (loading) {
+        setSlowInternet(true);
+      }
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+      clearTimeout(slowInternetTimeout);
+    };
   }, []);
+
+  const handleSlowInternetClose = () => {
+    setSlowInternet(false);
+  };
+
+  const handleNoInternetClose = () => {
+    setNoInternet(false);
+  };
 
   return (
     <div>
@@ -51,6 +81,16 @@ const Layout = ({ children }) => {
           )}
         </div>
       </div>
+      <Snackbar
+        open={slowInternet}
+        onClose={handleSlowInternetClose}
+        message="Slow internet connection detected."
+      />
+      <Snackbar
+        open={noInternet}
+        onClose={handleNoInternetClose}
+        message="No internet connection. Please check your network."
+      />
     </div>
   );
 };
